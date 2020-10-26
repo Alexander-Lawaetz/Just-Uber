e8d35809e0b88c24f76ef5b094c3728be618680c
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-10-26 13:15:11 +0100

    fortify setup

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -41,19 +41,18 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.register');
         });
 
-        Fortify::requestPasswordResetLinkView(function () {
-            return view('auth.forgot-password');
-        });
+        // Fortify::requestPasswordResetLinkView(function () {
+        //     return view('auth.forgot-password');
+        // });
 
-        Fortify::resetPasswordView(function ($request) {
-            return view('auth.reset-password', ['request' => $request]);
-        });
-
-        
-        Fortify::verifyEmailView(function () {
-            return view('auth.verify-email');
-        });
+        // Fortify::resetPasswordView(function ($request) {
+        //     return view('auth.reset-password', ['request' => $request]);
+        // });
 
         
+        // Fortify::verifyEmailView(function () {
+        //     return view('auth.verify-email');
+        // });
+
     }
 }
