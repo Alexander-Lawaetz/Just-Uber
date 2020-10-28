9596019f64a89a5f65ba14a25154018061354ea4
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-10-28 10:06:22 +0100

    update basic layout for testing purpose

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -23,7 +23,13 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <div class="flex flex-row">
+                            <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline px-2 py-1 button">Home</a>
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <input type="submit" class="text-sm ml-2 px-2 py-1 text-gray-700 underline" value="logout">
+                            </form>
+                        </div>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
