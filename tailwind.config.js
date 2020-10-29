10777b671d4ca3d55b2af26d21120b45e2d5a518
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-10-29 15:25:58 +0100

    UPDATE tailwind.config.js and resources/css/main.css to introduce color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    extend: {
-      screens: {
-        'dark': {'raw': '(prefers-color-scheme: dark)'},
-        // => @media (prefers-color-scheme: dark) { ... }
-      }
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
     },
-  },
-  variants: {},
-  plugins: [],
+    purge: [],
+    theme: {
+        extend: {
+            screens: {
+                'dark': {'raw': '(prefers-color-scheme: dark)'},
+                // => @media (prefers-color-scheme: dark) { ... }
+            },
+            backgroundColor: {
+                primary: 'var(--bg-color-primary)',
+                secondary: 'var(--bg-color-secondary)',
+                important: 'var(--bg-color-important)',
+                hover: 'var(--bg-color-hover)',
+            },
+            color: {
+                primary: 'var(--text-color-primary)',
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
 }
