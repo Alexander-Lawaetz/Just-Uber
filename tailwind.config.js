01b2592fd97030082af35055b274c3836c9b1a7b
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-11-05 12:25:15 +0100

    Tailwindcss config extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,26 @@ module.exports = {
                     important: 'var(--dark-text-color-important)',
                 },
             },
+            borderColor: {
+                dark: {
+                    primary: 'var(--dark-bg-color-primary)',
+                    secondary: 'var(--dark-bg-color-secondary)',
+                    important: 'var(--dark-bg-color-important)',
+                    hover: 'var(--dark-bg-color-hover)',
+                },
+            },
+            inset: {
+                '1/4': '25%',
+                '1/2': '50%',
+                '3/4': '75%',
+            },
+            height: {
+                72: '18rem',
+                104: '24rem',
+            },
+            maxHeight: {
+                '(screen-phone)': 'calc(100vw - 14rem)',
+            }
         },
     },
     variants: {},
