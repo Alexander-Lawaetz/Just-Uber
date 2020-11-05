014653be5955eeac1004424c8ac07c894af35753
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-11-05 15:19:02 +0100

    Footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,73 @@
+<div class="w-full">
+    <div class="py-4 sm:py-8 dark:bg-dark-secondary">
+        <div class="container px-4 sm:px-8 mx-auto">
+            <div class="flex">
+                <div class="flex-grow">
+                    <h2 class="text-xl font-semibold mb-4 leading-loose">Explore cuisines</h2>
+                    <div>
+                        <ul>
+                            <li><a href="#" class="py-2 hover:underline">Premium & Gourment</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Pizza</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Burgers</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Mexican</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Sushi</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Chinese</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Indian</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="flex-grow">
+                    <h2 class="text-xl font-semibold mb-4 leading-loose">Find your city</h2>
+                    <div>
+                        <ul>
+                            <li><a href="#" class="py-2 hover:underline">Copenhagen</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Roskilde</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Aarhus</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Odense</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Aalborg</a></li>
+                            <li><a href="#" class="py-2 hover:underline">More cities</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="flex-grow">
+                    <h2 class="text-xl font-semibold mb-4 leading-loose">About us</h2>
+                    <div>
+                        <ul>
+                            <li><a href="#" class="py-2 hover:underline">Coronavirus</a></li>
+                            <li><a href="#" class="py-2 hover:underline">About Just Uber</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Buy a Giftcard</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Price promise</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Restuarant sign up</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Partner Center</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Cookie Policy</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Privacy Policy</a></li>
+                            <li><a href="#" class="py-2 hover:underline">Terms and Conditions</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-4 sm:py-8 dark:bg-dark-primary">
+        <div class="container px-4 sm:px-8 mx-auto">
+            <div class="flex justify-between">
+                <div>
+                    <h2 class="text-xl font-semibold mb-4 leading-loose">Download our app</h2>
+                    <div class="flex flex-row justify-between sm:justify-center">
+                        <a href="#"><img src="{{ asset('storage/DK-app-store-icon.svg') }}"></a>
+                        <a href="#" class="sm:ml-4"><img src="{{ asset('storage/DK-google-play-icon.svg') }}"></a>
+                    </div>
+                </div>
+                <div>
+                    <h2 class="text-xl font-semibold mb-4 leading-loose">Follow Us</h2>
+                    <div class="flex flex-row justify-between sm:justify-center">
+                        <a href="#"><x-blogsvg class="dark:text-color-primary"/></a>
+                        <a href="#" class="sm:ml-4"><x-facebooksvg class="dark:text-color-primary"/></a>
+                        <a href="#" class="sm:ml-4"><x-twittersvg class="dark:text-color-primary"/></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
