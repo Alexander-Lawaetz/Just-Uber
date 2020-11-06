8475cc1fc3f36708f992e3906bbdc774fd7c2c74
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-11-06 11:34:38 +0100

    UPDATE resources/views/components/footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="w-full">
     <div class="py-4 sm:py-8 dark:bg-dark-secondary">
         <div class="container px-4 sm:px-8 mx-auto">
-            <div class="flex">
-                <div class="flex-grow">
+            <div class="flex flex-col sm:flex-row">
+                <div class="flex-grow mb-4">
                     <h2 class="text-xl font-semibold mb-4 leading-loose">Explore cuisines</h2>
                     <div>
                         <ul>
@@ -16,7 +16,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="flex-grow">
+                <div class="flex-grow mb-4">
                     <h2 class="text-xl font-semibold mb-4 leading-loose">Find your city</h2>
                     <div>
                         <ul>
@@ -26,11 +26,10 @@
                             <li><a href="#" class="py-2 hover:underline">Odense</a></li>
                             <li><a href="#" class="py-2 hover:underline">Aalborg</a></li>
                             <li><a href="#" class="py-2 hover:underline">More cities</a></li>
-
                         </ul>
                     </div>
                 </div>
-                <div class="flex-grow">
+                <div class="flex-grow mb-4">
                     <h2 class="text-xl font-semibold mb-4 leading-loose">About us</h2>
                     <div>
                         <ul>
@@ -51,20 +50,44 @@
     </div>
     <div class="py-4 sm:py-8 dark:bg-dark-primary">
         <div class="container px-4 sm:px-8 mx-auto">
-            <div class="flex justify-between">
-                <div>
+            <div class="sm:flex sm:justify-between">
+                <div class="mb-4">
                     <h2 class="text-xl font-semibold mb-4 leading-loose">Download our app</h2>
-                    <div class="flex flex-row justify-between sm:justify-center">
+                    <div class="flex flex-row sm:justify-between justify-items-start">
                         <a href="#"><img src="{{ asset('storage/DK-app-store-icon.svg') }}"></a>
-                        <a href="#" class="sm:ml-4"><img src="{{ asset('storage/DK-google-play-icon.svg') }}"></a>
+                        <a href="#" class="ml-4"><img src="{{ asset('storage/DK-google-play-icon.svg') }}"></a>
                     </div>
                 </div>
                 <div>
                     <h2 class="text-xl font-semibold mb-4 leading-loose">Follow Us</h2>
-                    <div class="flex flex-row justify-between sm:justify-center">
-                        <a href="#"><x-blogsvg class="dark:text-color-primary"/></a>
-                        <a href="#" class="sm:ml-4"><x-facebooksvg class="dark:text-color-primary"/></a>
-                        <a href="#" class="sm:ml-4"><x-twittersvg class="dark:text-color-primary"/></a>
+                    <div class="flex flex-row sm:justify-between justify-items-start">
+                        <a href="#"><x-blogsvg class="dark:text-dark-important"/></a>
+                        <a href="#" class="ml-4"><x-facebooksvg class="dark:text-dark-important"/></a>
+                        <a href="#" class="ml-4"><x-twittersvg class="dark:text-dark-important"/></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sm:py-4 sm:py-8 dark:bg-dark-secondary">
+        <div class="sm:container sm:px-4 sm:px-8 sm:mx-auto">
+            <div class="flex flex-col sm:flex-row sm:justify-between">
+                <div class="sm:p-0 order-last sm:order-first">
+                    <button class="hover:underline h-full">
+                        <div class="flex">
+                            <span><img class="h-4" src="{{ asset('storage/flag-icons/denmark.svg') }}" /></span>
+                            <div class="flex items-center">
+                                <span class="ml-2">Danmark</span>
+                                <span class="ml-2 w-4 h-4"><svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></span>
+                            </div>
+                        </div>
+                    </button>
+                </div>
+                <div class="p-4 sm:p-0 sm:border-none border-b border-dark-primary">
+                    <div class="flex flex-row h-8">
+                        <img class="h-full" src="{{ asset('storage/dk-payment.svg') }}" />
+                        <img class="ml-4 h-full" src="{{ asset('storage/je-payment-logos-mastercard.svg') }}" />
+                        <img class="ml-4 h-full" src="{{ asset('storage/je-payment-logos-visa.svg') }}" />
                     </div>
                 </div>
             </div>
