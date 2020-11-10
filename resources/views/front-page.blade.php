ceee8bf414e680d9928f12b3ff1d1bb23b60b0a2
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-11-10 10:15:13 +0100

    UPDATE Fixed undesired width proportions by adding icon svg span for phone users

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -23,9 +23,13 @@
                         @csrf
                         <label class="w-full min-h-full">
                             <input id="zipSearch" class="w-full min-h-full px-4 pt-6 pb-2 text-bold dark:bg-dark-primary dark:text-dark-primary" onkeydown="return checkPhoneKey(event.key)" style="" id="search-input" type="text" autocomplete="false" pattern="\d{1,4}"  maxlength="4" title="Four digit zip code" required>
-                            <span class="absolute top-1/4 left-0 ml-4 mt-1 transition-all duration-500 ease-in-out ">Enter your postcode</span>
+                            <span class="absolute top-1/4 left-0 ml-4 mt-1 transition-all duration-500 ease-in-out text-sm sm:text-base">Enter your postcode</span>
                         </label>
-                        <input class="lg:text-xl text-base leading-relaxed tracking-wide font-bold px-4 py-2 cursor-pointer text-light-secondary bg-light-important dark:bg-dark-important" type="submit" value="Find restaurants">
+                        <button class="bg-light-important dark:bg-dark-important hover:bg-light-hover dark:hover:bg-dark-hover text-light-secondary font-bold py-2 px-4 inline-flex items-center">
+                            //https://heroicons.dev/
+                            <svg class="sm:hidden w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                            <span class="hidden sm:block whitespace-no-wrap">Find restaurant</span>
+                        </button>
                     </form>
                 </div>
             </div>
