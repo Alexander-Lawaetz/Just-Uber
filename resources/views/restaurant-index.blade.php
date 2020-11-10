66394b03ece02837503a614aecab1483a639be96
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2020-11-10 12:54:45 +0100

    ADD preperations for the next step of the project, which is creating the Find Restaurants near me view

---
+++ resources/views/restaurant-index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.layout')
+
+@section('title', 'Food delivery & Take-away | Just Uber')
+
+@section('navbar')
+    <header class="container px-4 mx-auto">
+        <x-navbar class="h-16"/>
+    </header>
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('footer')
+    <x-footer />
+@endsection
