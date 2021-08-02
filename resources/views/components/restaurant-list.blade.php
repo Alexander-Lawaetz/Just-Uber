a868a056ee3a45e552952623c60c3b0d21883cae
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2021-08-02 08:35:26 +0200

    UPDATE minor tweaks

--- resources/views/components/restaurant-list.blade.php
+++ resources/views/components/restaurant-list.blade.php
@@ -4,7 +4,7 @@
         <div class="flex-col divide-y divide-gray-600">
             @forelse($restaurants as $restaurant)
                 <div class="py-4">
-                    <a class="hover:bg-blue-400" href=#{{ $restaurant->name }}>
+                    <a class="hover:bg-blue-400" href="#/restaurants-{{ $restaurant->name }}">
                         <div class="flex flex-col sm:flex-row h-full w-full">
                             <div class="relative h-32 sm:w-52 sm:mr-12 mb-6 sm:mb-0">
                                 <div class="h-full w-full bg-cover bg-no-repeat bg-center" style="background-image: url({{ asset('/storage/15888.jpg') }})"></div>
@@ -15,28 +15,31 @@
                                     <h2 class="pb-1 font-bold">{{ $restaurant->name }}</h2>
                                     <div class="flex items-center pb-1">
                                         @for($i = 0; $i < 6; $i++)
-                                            <x-icons.star-solid-svg class="h-4 w-4 {{ $i <= round($restaurant->reputation->avg_stars) ? 'text-light-important dark:text-dark-important' : 'text-gray-400'}}" />
+                                            <x-icons.star-solid-svg class="h-4 w-4 {{ $i <= round($restaurant->reputation->avg_stars) ? 'text-light-important dark:text-dark-important' : 'text-gray-400'}}"></x-icons.star-solid-svg>
                                         @endfor
                                         <span class="ml-2 text-xs">({{ $restaurant->reputation->ratings }})</span>
                                     </div>
-                                    <p class="text-sm font-bold">{{ implode(', ', $restaurant->main_dishes) }}</p>
+                                    <p class="text-sm font-bold capitalize">{{ implode(', ', $restaurant->main_dishes) }}</p>
                                 </div>
                                 <div class="w-full h-full sm:pl-2">
                                     <div class="flex flex-col justify-between h-32 text-left my-auto text-sm">
                                         <div class="flex items-center mt-6">
                                             @if(empty($restaurant->details->take_away))
-                                                <x-icons.just-eat.self-pickup-svg class="h-6 w-6" />
+                                                <x-icons.just-eat.self-pickup-svg class="h-6 w-6"></x-icons.just-eat.self-pickup-svg>
                                                 <p class="ml-2">Only self pickup</p>
                                             @else
-                                                <x-icons.just-eat.money-svg class="h-6 w-6" />
-                                                {{-- TODO fix shorten this statement --}}
+                                                <x-icons.just-eat.money-svg class="h-6 w-6"></x-icons.just-eat.money-svg>
+                                                {{-- TODO shorten this statement --}}
                                                 <p class="ml-2">
                                                     Delivery {{ !empty($restaurant->details->take_away->deliver_fee) ? $restaurant->details->take_away->deliver_fee . ' ' . $restaurant->details->take_away->currency_sign . '.' : 'FREE' }}
                                                     - {{ !empty($restaurant->details->take_away->min_order) ? 'Min. order ' . $restaurant->details->take_away->min_order . ' ' . $restaurant->details->take_away->currency_sign : 'No min. order' }}
                                                 </p>
                                             @endif
                                         </div>
-                                        <p>Opening hours: </br> {{ implode(' - ', $restaurant->details->opening_hours) }}</p>
+                                        <div>
+                                            <p>Opening hours:</p>
+                                            <p>{{ implode(' - ', $restaurant->details->opening_hours) }}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -44,7 +47,7 @@
                     </a>
                 </div>
             @empty
-                <div class="text-2xl text-center">No restaurants - Internal error</div>
+                <div class="text-2xl text-center">No results</div>
             @endforelse
         </div>
     </div>
