7cef062190b70244c3825a95eb1e825962fe6cb8
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2021-08-23 08:42:35 +0200

    Restaurant List component

---
+++ app/Http/Livewire/RestaurantList.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RestaurantList extends Component
+{
+
+
+
+
+    public function render()
+    {
+        return view('livewire.restaurant-list');
+    }
+}
