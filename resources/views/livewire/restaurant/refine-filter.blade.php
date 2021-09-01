db6a55409149fa38cb7872bb4df9606aee81461a
Alexander Lawaetz <alexander.enetoft.lawaetz@gmail.com>
2021-09-01 08:28:10 +0200

    minor prettier modifications

--- resources/views/livewire/restaurant/refine-filter.blade.php
+++ resources/views/livewire/restaurant/refine-filter.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div
-        class="flex justify-between mb-4 min-w-full w-full px-4 py-2 mx-auto cursor-pointer lg:cursor-text text-xl my-1 font-light"
+        class="flex justify-between mb-4 min-w-full w-full px-2 py-2 mx-auto cursor-pointer lg:cursor-text text-xl my-1 font-light"
         }}>
         <h2 class="flex capitalize">
             <x-icons.just-eat.cuisine-svg class="h-6 w-6 mr-2"/>
@@ -32,15 +32,15 @@
                 </label>
             </div>
             @if ($loop->iteration > 8 && $loop->last)
-                <div x-data="{ open: false }">
+                <div x-data="{ open: false }" class="px-2 py-2">
                     <button
                         name="expand-button"
                         type="button"
-                        class="flex items-center"
                         @click="open = !open"
+                        x-text="open ? 'View less' : 'View more'"
                         wire:click="expand"
-                    >View more <span :class="open ? '' : 'rotate-180'" class="transform"><x-icons.chevron-down-outline-svg class="h-4 w-4" /></span>
-                    </button>
+                    ></button>
+                    <span x-bind:class="open ? 'rotate-180' : ''" class="transform px-2 py-2"><x-icons.chevron-down-outline-svg class="h-4 w-4"/></span>
                 </div>
             @endif
         @empty
